c79a425f83c59b7c67d7b7104c119b793a482da8
DhruvTalwarr <156096641+DhruvTalwarr@users.noreply.github.com>
2026-09-15 10:09:11 +0530

    Add Medium C++ Solution for 743. Network Delay Time from LeetCode

---
+++ LeetCode/C++/Medium/743. Network Delay Time/Solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int src) {
+        vector<vector<pair<int, int>>> a(n);
+        for(int i = 0 ; i < times.size() ; i++){
+            int s = times[i][0];
+            int d = times[i][1];
+            int w = times[i][2];
+
+            a[s - 1].push_back({d - 1, w});
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        vector<int> dist(n, INT_MAX);
+        dist[src - 1] = 0;
+        pq.push({0, src - 1});
+
+        while(!pq.empty()){
+            pair<int, int> p = pq.top();
+            pq.pop();
+            int d = p.first;
+            int node = p.second;
+            
+
+            if(d > dist[node]){
+                continue;
+            }
+            for(int j = 0 ; j < a[node].size() ; j++){
+                int neigh = a[node][j].first;
+                int wt = a[node][j].second;
+
+                if(d + wt < dist[neigh]){
+                    dist[neigh] = d + wt;
+                    pq.push({d + wt, neigh});
+                }
+            }
+        }
+
+
+        int maximum = 0;
+        for (int i = 0; i < n; i++) {
+            if (dist[i] == INT_MAX) return -1;
+            maximum = max(maximum, dist[i]);
+        }
+        return maximum;
+    }
+};

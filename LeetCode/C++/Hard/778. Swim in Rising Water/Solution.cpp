031b365e7f862a55a286ae3bb98938ef9f2fff5a
DhruvTalwarr <156096641+DhruvTalwarr@users.noreply.github.com>
2026-09-19 18:55:53 +0530

    Add Hard C++ Solution for 778. Swim in Rising Water from LeetCode

---
+++ LeetCode/C++/Hard/778. Swim in Rising Water/Solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    int x[4] = {1, -1, 0, 0};
+    int y[4] = {0, 0, -1, 1};
+
+    bool valid(int i, int j, int n, int m){
+        if(i < 0 || i >= n || j < 0 || j >= m) return false;
+        return true;
+    }
+
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int,int>>>> pq;
+
+        vector<vector<int>> res(n, vector<int>(m, 1e8));
+        int i, j;
+
+        // for(i = 0 ; i < n ; i++){
+        //     vector<int> t(m, 1e8);
+        // }
+
+        pq.push(make_pair(grid[0][0], make_pair(0, 0)));
+        res[0][0] = grid[0][0];
+
+        while(!pq.empty()){
+            pair<int, pair<int,int>> p = pq.top();
+            pq.pop();
+            int money = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            if(money > res[row][col]) continue;
+
+            for(int k = 0 ; k < 4 ; k++){
+                int r = row + x[k];
+                int c = col + y[k];
+
+                if(!valid(r, c, n, m)) continue;
+
+                int new_money = max(money, grid[r][c]);
+                if(new_money < res[r][c]){
+                    res[r][c] = new_money;
+                    pq.push({new_money, {r, c}});
+                }
+            }
+        }
+
+        return res[n - 1][m - 1];
+    }
+};

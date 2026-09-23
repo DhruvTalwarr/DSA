f8893b2f0b165d6a1abea36eaae60a3778a660bd
DhruvTalwarr <156096641+DhruvTalwarr@users.noreply.github.com>
2026-09-23 21:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (37.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,40 @@
 class Solution {
 public:
+    unordered_map<int, int> dp;
     int fib(int n) {
-        if(n == 0)
-        return 0;
-        if(n == 1) 
-        return 1;
 
-        return fib(n - 1) + fib(n - 2);
+        //recursion
+        // if(n == 0)
+        // return 0;
+        // if(n == 1) 
+        // return 1;
+
+        // return fib(n - 1) + fib(n - 2);
+
+        //dp(memoization)
+        // if(n == 0 || n == 1) return n;
+
+        // if(dp.find(n) != dp.end()) return dp[n];
+
+        // int a1 = fib(n - 1);
+        // int a2 = fib(n - 2);
+
+        // int ans = a1 + a2;
+        // dp[n] = ans;
+
+        // return ans;
+        
+        //DP(tabulation)
+        if(n == 0 || n == 1) return n;
+        int prev = 1;
+        int prev_prev = 0;
+
+        for(int i = 2 ; i <= n ; i++){
+            int ans = prev + prev_prev;
+            prev_prev = prev;
+            prev = ans;
+        }
+        return prev;
+
     }
 };
